4be013b955b0ac075bf9940429a7f55ab54da829
Ian Cooper & Matt Mills <pair+ian.cooper+matthew.mills@socialchorus.com>
2014-05-15 16:10:34 -0700

    Fixes tasks...

--- lib/superbolt/future/tasks.rb
+++ lib/superbolt/future/tasks.rb
@@ -1,3 +1,1 @@
-require 'superbolt-future'
-
-load "#{File.dirname(__FILE__)}/../tasks/superbolt_future.rake"
+load "#{File.dirname(__FILE__)}/../../tasks/superbolt_future.rake"
--- lib/superbolt/future/version.rb
+++ lib/superbolt/future/version.rb
@@ -1,5 +1,5 @@
 module Superbolt
   module Future
-    VERSION = "0.1.1"
+    VERSION = "0.1.2"
   end
 end
